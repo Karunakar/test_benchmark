21a0a15877d753526c0b4348644484768314a752
Karunakar <revurikarna@gmail.com>
2010-08-03 19:30:35 +0530

    Collecting the top most benchmark results and dump inot the text file

--- lib/test_benchmark.rb
+++ lib/test_benchmark.rb
@@ -1,41 +1,30 @@
+unless %w{false none}.include?(ENV['BENCHMARK'])
+
 require 'test/unit'
 require 'test/unit/testresult'
 require 'test/unit/testcase'
 require 'test/unit/ui/console/testrunner'
 
 class Test::Unit::UI::Console::TestRunner
-  DEFAULT_DISPLAY_LIMIT = 15
-  DEFAULT_SUITE_DISPLAY_LIMIT = 5
-  
-  @@display_limit = DEFAULT_DISPLAY_LIMIT
-  @@suite_display_limit = DEFAULT_SUITE_DISPLAY_LIMIT
-  
-  def self.set_test_benchmark_limits(set_display_limit=DEFAULT_DISPLAY_LIMIT, set_suite_display_limit=DEFAULT_SUITE_DISPLAY_LIMIT)
-    @@display_limit = set_display_limit
-    @@suite_display_limit = set_suite_display_limit
-  end
-  
+  DISPLAY_LIMIT = 15
+  SUITE_DISPLAY_LIMIT = 5
+
+  @@max_time_limit = nil
+  @@threshold_file = nil
+
   alias attach_to_mediator_old attach_to_mediator
-  # def attach_to_mediator_old
-  #   @mediator.add_listener(TestResult::FAULT, &method(:add_fault))
-  #   @mediator.add_listener(TestRunnerMediator::STARTED, &method(:started))
-  #   @mediator.add_listener(TestRunnerMediator::FINISHED, &method(:finished))
-  #   @mediator.add_listener(TestCase::STARTED, &method(:test_started))
-  #   @mediator.add_listener(TestCase::FINISHED, &method(:test_finished))
-  # end
+ 
   def attach_to_mediator
     attach_to_mediator_old
     @mediator.add_listener(Test::Unit::TestSuite::STARTED, &method(:test_suite_started))
     @mediator.add_listener(Test::Unit::TestSuite::FINISHED, &method(:test_suite_finished))
   end
-  
-  def add_fault(fault)
-    @faults << fault
-    nl
-    output("%3d) %s" % [@faults.length, fault.long_display])
-    @already_outputted = true
+
+  def self.set_benchmark_threshold_in_seconds(max_time_limit, threshold_file = "test_benchmark.txt")
+    @@max_time_limit = max_time_limit
+    @@threshold_file = threshold_file
   end
-  
+
   alias started_old started
   def started(result)
     started_old(result)
@@ -45,21 +34,17 @@ class Test::Unit::UI::Console::TestRunner
   
   alias finished_old finished
   def finished(elapsed_time)
-    nl
-    output("Finished in #{elapsed_time} seconds.")
-    set_terminal_title("Finished in #{elapsed_time} seconds.")
-    output(@result)
+    finished_old(elapsed_time)
     output_benchmarks
     output_benchmarks(:suite)
-    nl
-    exit!(1) unless @faults.empty?
+    dump_into_file(:suite) if @@max_time_limit
+    puts "\n"
   end
   
   alias test_started_old test_started
   def test_started(name)
     test_started_old(name)
     @test_benchmarks[name] = Time.now
-    set_terminal_title(name)
   end
   
   alias test_finished_old test_finished
@@ -81,11 +66,6 @@ class Test::Unit::UI::Console::TestRunner
   @@sort_by_time = lambda { |a,b| b[1] <=> a[1] }
 
 private
-  def set_terminal_title(string)
-    print "\033]0;#{string}\007"
-  end
-
-
   def full_output?
     ENV['BENCHMARK'] == 'full'
   end
@@ -105,7 +85,7 @@ private
     return if benchmarks.nil? || benchmarks.empty?
     benchmarks = benchmarks.sort(&@@sort_by_time)
     unless full_output?
-      cutoff = (suite_name == :suite) ? @@suite_display_limit : @@display_limit
+      cutoff = (suite_name == :suite) ? SUITE_DISPLAY_LIMIT : DISPLAY_LIMIT
       benchmarks = benchmarks.slice(0, cutoff) 
     end
     benchmarks.map(&@@format_benchmark_row)
@@ -124,6 +104,23 @@ private
   def output_benchmarks(suite_name=nil)
     benchmarks = prep_benchmarks(suite_name)
     return if benchmarks.nil? || benchmarks.empty?
-    output(header(suite_name) + benchmarks.join("\n") + "\n")
+    puts header(suite_name) + benchmarks.join("\n") + "\n"
   end
+
+  def dump_into_file(suite_name=nil)
+    benchmarks = prep_benchmarks(suite_name)
+    benchmark_file = []
+    csv_string = File.open(@@threshold_file,"a") do |csv|
+    benchmarks.each do |b|
+       csv << b  if exceeds_time_limit?(b)
+     end
+    end
+  end
+
+  def exceeds_time_limit?(b)
+    @@max_time_limit.to_i <= b.split(" ").first.to_i
+  end
+
+end
+
 end
